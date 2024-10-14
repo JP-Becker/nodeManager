2af427f44ec3e53c9c42e588e1b20696d9b0ea18
JP-Becker <jpbecker20@gmail.com>
2024-10-14 20:15:26 -0300

    passando props pro nodemenu

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect} from 'react';
+import React, { useCallback, useEffect } from 'react';
 import {
   ReactFlow,
   MiniMap,
@@ -9,51 +9,50 @@ import {
   Background
 } from '@xyflow/react';
 import NodeMenu from './nodeTypes/nodeMenu';
-import {nodeData} from './nodeData/nodeData'; 
- 
+import { nodeData } from './nodeData/nodeData';
+
 import '@xyflow/react/dist/style.css';
 
-const teste = {
-  "id": "a2235f93-ab20-412a-8aa0-d16025c00af3",
-  "type": "MENU",
-  "nextNodeId": null,
-  "content": {
-    "text": "Sobre qual assunto você quer conversar?"
-  }}
+const nodeTypes = { nodeMenu: NodeMenu };
 
-  const nodesTypeMenu = nodeData.filter((node) => node.type === 'MENU');
+const nodesTypeMenu = nodeData.filter((node) => node.type === 'MENU');
 
-  // Gerar nodes a partir do NodeMenu
-  const menuNodes = nodesTypeMenu.map((node, index) => ({
-    id: (index+1).toString(),
-    position: { x: 0, y: index * 100 },
-    data: { label: `${node.type} \n ${node.content.text}` }
-  }));
+// Gerar nodes a partir do NodeMenu
+const menuNodes = nodesTypeMenu.map((node, index) => ({
+  id: node.id,
+  position: { x: 0, y: index * 100 },
+  type: 'nodeMenu',
+  data: { label: `${node.type} \n ${node.content.text}` }
+}));
 
 const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
- 
+
+
+
 export default function App() {
   const [nodes, setNodes, onNodesChange] = useNodesState([]);
   const [edges, setEdges, onEdgesChange] = useEdgesState();
- 
+
   const onConnect = useCallback(
     (params) => setEdges((eds) => addEdge(params, eds)),
     [setEdges],
   );
   useEffect(() => {
     setNodes(menuNodes)
-    console.log(menuNodes)
+    console.log(menuNodes.map((node) => node.data))
   }, [])
 
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <ReactFlow
+        // colorMode=''
         nodes={nodes}
         edges={edges}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        nodeTypes={nodeTypes}
       >
         <Controls />
         <MiniMap />
--- src/nodeTypes/nodeMenu.js
+++ src/nodeTypes/nodeMenu.js
@@ -2,26 +2,10 @@ import React, { memo } from 'react';
 import { nodeData } from '../nodeData/nodeData';
 import { Handle, Position } from '@xyflow/react';
 
-export default function NodeMenu() {
-    const nodesTypeMenu = nodeData.filter((node) => node.type == 'MENU');
+export default function NodeMenu({ data }) {
+    console.log(data);
 
-    const menuNodes = nodesTypeMenu.map((node, index) => ({
-        id: node.id,
-        position: node.position || { x: 0, y: index * 100 },
-        data: { label: `${node.type} \n ${node.content.text}` }
-      }));
-
-    return <>
-        {nodesTypeMenu.map((node) => (
-            <div key={node.id} style={{ border: '1px solid black', margin: '10px' }}>
-                <Handle
-                    type="target"
-                    position={Position.Left}
-                    style={{ background: '#555' }}
-                    onConnect={(params) => console.log('handle onConnect', params)}
-                />
-                <div>{`${node.type} \n ${node.content.text}`}</div>
-            </div>
-        ))}
-    </>
+    return <div>
+        <p>{data.label}</p>        
+    </div>
 }
